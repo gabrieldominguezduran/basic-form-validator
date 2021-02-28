6888c2e2ebb661cb1a413ece2e1933bea55bdeac
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2021-02-28 20:08:48 +0100

    Add validation

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const password2 = document.querySelector("#password2");
 const checkbox = document.querySelector("#checkbox");
 const show = document.querySelector(".show");
 
-const passRegEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-
 // Check valid email
-const isValidEmail = (email) => {
+const checkEmail = (input) => {
   const emailRegEx = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  if (emailRegEx.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+};
 
-  return emailRegEx.test(email.value);
+// Check valid password
+const checkPassword = (input) => {
+  const passRegEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{6,}/;
+
+  if (passRegEx.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Password not valid");
+  }
 };
 
 //Error message
@@ -40,6 +52,30 @@ const checkRequired = (inputArr) => {
   });
 };
 
+//Check input length
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less then ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+// Check passwords match
+const checkPasswordsMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+};
+
 //Get field name
 const getFieldName = (input) => {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -50,6 +86,11 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPassword(password);
+  checkPasswordsMatch(password, password2);
 });
 
 // Show password listener
